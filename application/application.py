7d4763768047e075d753115ed57974045bd1771e
Torbjørn <torbrin11@gmail.com>
2025-04-01 15:07:55 +0200

    Enhance scooter status handling and application state management

--- application/application.py
+++ application/application.py
@@ -5,18 +5,55 @@ broker, port = "mqtt20.iik.ntnu.no", 1883
 
 class Application:
     stm: stmpy.Machine 
+    known_scooters = {}
 
     def __init__(self, username: str):
         self.username = username
+
         self.client = mqtt.Client()
         self.client.connect(broker, port)
-        self.client.subscribe('gr8/scooters')
+
+        self.client.subscribe('gr8/scooters/status')
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
 
     def on_connect(self, client, userdata, flags, rc):
         print(f"{self.username}'s application connected to server")
 
     def on_message(self, client, userdata, msg):
-        print(f"{self.username}'s application received message: {msg.payload}")
+        # print(f"{self.username}'s application received message: {msg.payload}")
+        try:
+            payload = json.loads(msg.payload.decode("utf-8"))
+        except Exception as err:
+            print(f"[ERROR {self.scooter_id}] on_message(): {err}")
+            return
+        
+        #sort on topic
+        if msg.topic == 'gr8/scooters/status':
+            scooter_id = payload.get('scooter_id')
+            status = payload.get('status')
+            location = payload.get('location')
+            battery = payload.get('battery')
+            
+            scooter = {
+                'status': status,
+                'location': location,
+                'battery': battery
+            }
+
+            if scooter_id not in self.known_scooters and status == 'available' and battery > 20:
+                self.known_scooters[scooter_id] = scooter
+                # print(f"Scooter added: {scooter_id}")
+            elif scooter_id in self.known_scooters and status != 'available':
+                del self.known_scooters[scooter_id]
+                # print(f"Scooter removed: {scooter_id}")
+    
+
+    def get_scooter_list(self):
+        self.client.publish('gr8/server/scooter_list', 'Go', qos=1)
+
+            
+
 
     # def reserve_scooter(self, scooter_id):
     #     print("reserve_scooter()")
@@ -33,52 +70,29 @@ class Application:
         # publish to mqtt
         pass
 
-    def unlock_scooter(self, scooter_id):
+    def unlock(self):
         print("unlock_scooter()")
         # publish to mqtt
         pass
 
-    def park_scooter(self, scooter_id):
-        print("park_scooter()")
-        # publish to mqtt
-        pass
-
-    def calculate_bill(self, scooter_id):
-        print("calculate_bill()")
-        # publish to mqtt
-        pass
-
-    def complete_transaction(self, scooter_id):
-        print("complete_transaction()")
-        # publish to mqtt
-        pass
-
-    def file_report(self, scooter_id, contents):
+    def file_report(self):
         print("file_report()")
-        
+        #publish to mqtt
+        pass
+
+    def lock(self):
+        print("lock_scooter()")
+        # publish to mqtt
         pass
 
 application_states = [
     {
-        'name': 'reserved',
-        'entry': 'reserve_scooter'
-    },
-    {
-        'name': 'driving',
-        'entry': 'unlock_scooter'
-    },
-    {
-        'name': 'park_scooter',
-        'entry': 'park_scooter'
-    },
-    {
-        'name': 'calculate_bill_and_pay',
-        'entry': 'calculate_bill',
-        'exit': 'complete_transaction'
-    },
-    {
         'name': 'idle',
-    }
+        'entry': 'list_available_scooters'
+    },
+    {
+        'name': 'active',
+    },
 ]
 
 application_transitions = [
@@ -88,45 +102,27 @@ application_transitions = [
     },
     {
         'source': 'idle',
-        'target': 'reserved',
-        'trigger': 'reserve'
-    },
-    {
-        'source': 'reserved',
-        'target': 'driving',
-        'trigger': 'unlock'
-    },
-    {
-        'source': 'idle',
-        'target': 'driving',
-        'trigger': 'unlock'
-    },
-    {
-        'source': 'driving',
-        'target': 'park_scooter',
-        'trigger': 'park'
-    },
-    {
-        'source': 'park_scooter',
-        'target': 'calculate_bill_and_pay',
-        'trigger': 'recive'
-    },
-    {
-        'source': 'calculate_bill_and_pay',
-        'target': 'idle',
-        'trigger': 'pay'
+        'target': 'active',
+        'trigger': 'unlock',
+        'effect': 'unlock'
     },
     {
         'source': 'idle',
         'target': 'idle',
         'trigger': 'report',
-        'action': 'file_report'
+        'effect': 'file_report'
     },
     {
-        'source': 'reserved',
-        'target': 'reserved',
-        'trigger': 'report',
-        'action': 'file_report'
+        'source': 'idle',
+        'target': 'idle',
+        'trigger': 'reserve',
+        'effect': 'file_report'
+    },
+    {
+        'source': 'active',
+        'target': 'idle',
+        'trigger': 'lock',
+        'effect': 'lock'
     },
 ]
 
