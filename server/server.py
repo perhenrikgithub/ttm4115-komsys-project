5cc3a4087c981466227d6f3a7690725ad97ebbea
Per Henrik mork Viken <perhenrikmv@gmail.com>
2025-04-03 16:54:30 +0200

    added server functions, some small changes to escooter

--- server/server.py
+++ server/server.py
@@ -1,43 +1,42 @@
-import stmpy
 import paho.mqtt.client as mqtt
 import json
 broker, port = "mqtt20.iik.ntnu.no", 1883
 
-
-class Server:
-    
-    users = []
-    scooters = []
-
+class Server:    
+    known_scooters = {}
 
     def __init__(self):
-        print("Server is initialized")
-
-        # mqtt stuff
         self.client = mqtt.Client()
         self.client.connect(broker, port)
-        self.client.subscribe('gr8/#') # TODO: add topic
+        self.client.subscribe('gr8/server/#')
+        self.client.subscribe('gr8/scooters/status')
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
+        self.client.loop_start()
 
     def on_connect(self, client, userdata, flags, rc):
         print(f"Server connected to broker")
 
     def on_message(self, client, userdata, msg):
-        print(f"Server received message: {msg.payload}")
+        if msg.topic == "gr8/server/scooter_list":
+            self.publish_scooter_list()
 
-        m = json.loads(msg.payload)
-        print(f"[SERVER] {m}")
+        elif msg.topic == "gr8/scooters/status":
+            try:
+                payload = json.loads(msg.payload.decode("utf-8"))
+            except Exception as err:
+                print(f"[ERROR {self.scooter_id}] on_message(): {err}")
+                return
+            
+            print(f"Received scooter status: {payload}")
 
-    def reserve_scooter(self):
-        pass
-
-    def unreserve_scooter(self):
-        pass
-
-    def unlock_scooter(self):
-        self.client.publish('gr8/scooters/action/1', json.dumps({'action': 'unlock'}))
-
-    def lock_scooter(self):
-        self.client.publish('gr8/scooters/action/1', json.dumps({'action': 'lock'}))
+            self.known_scooters[payload.get('scooter_id')] = {
+                'available': payload.get('available'),
+                'location': payload.get('location'),
+                'battery': payload.get('battery')    ,
+                'is_currently_charging': payload.get('is_currently_charging')
+            }
+    
+    def publish_scooter_list(self):
+        self.client.publish("gr8/scooters/scooter_list", json.dumps(self.known_scooters))
 
